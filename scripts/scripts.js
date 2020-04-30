89f90b71cab72f9c6d987fb4968d6ab1cc72c461
NorbyYT1300 <norbyyt1300@gmail.com>
2020-04-30 15:50:30 -0500

    Added faction filters

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -419,6 +419,7 @@ function loadDialsFromXWS(cleansedJSONFromURL) {
     document.getElementById("loadShipsButton").style.display = "none";
     document.getElementById("saveToURLButton").style.display = "none";
     document.getElementById("projectSourceLink").style.display = "none";
+    document.getElementById("main-top-nav-bar").style.display = "none";
 }
 
 // ------------------------------------------------------------------
@@ -494,5 +495,26 @@ function saveSquadToURL() {
     // If the URL search doesn't equal the new JSON, update it!
     if (window.location.search != JSON.stringify(squadJSON)) {
         window.location.search = JSON.stringify(squadJSON);
+    }
+}
+// Filter the list of dials!
+function filterDials(filterText) {
+    console.log("Filtering to only show dials for:", filterText);
+    // Get all options
+    var selectOptions = document.getElementsByClassName("shipTypeOptionElement");
+    // Loop and toggle their visibility
+    var first = true;
+    for (var i = 0; i < selectOptions.length; i++) {
+        // Ff this select does not contain the filter text, then hide it
+        if (selectOptions[i].innerHTML.indexOf(filterText) == -1 ) {
+            selectOptions[i].style.display = "none";
+        } else {
+            // Otherwise, show this option
+            selectOptions[i].style.display = "block";
+            if (first) {
+                document.getElementById("shipSelectorDropDown").selectedIndex = i + 1;
+                first = false;
+            }
+        }
     }
 }
